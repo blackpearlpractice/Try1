8f7547aa7037c0a1151925083d01c0339506ebe9
“[shanmugaperl1471]” <[shanmuga2link@gmail.com]>
2023-11-04 21:41:44 +0530

    2nd File added to new repo

---
+++ HelloWorld/src/Datatype_Operations.java
@@ -0,0 +1,17 @@
+public class Datatype_Operations 
+{
+public static void main(String[] args) 	
+{
+int a = 10;
+int b = 15;
+int c = a+b;
+float d = 5.3f;
+float e = 2.5f;
+float f = d+e;
+System.out.println("integer output = "+c);
+System.out.println("float output = "+f);
+
+
+}
+
+}
